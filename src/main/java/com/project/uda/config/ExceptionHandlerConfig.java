eac63df97111a10a4954500b5690035296813aba
[UC]박명국 <mkpark1@douzone.com>
2023-09-23 17:31:48 +0900

    🛠️Refactor : 전역 익셉션 처리 리팩터링

--- src/main/java/com/project/uda/config/ExceptionHandlerConfig.java
+++ src/main/java/com/project/uda/config/ExceptionHandlerConfig.java
@@ -2,24 +2,22 @@ package com.project.uda.config;
 
 import com.project.uda.dto.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.charset.StandardCharsets;
-
 @RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerConfig {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity rootExceptionHandler(Exception e){
-        log.error(e.getMessage(), e.getCause());
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-        return new ResponseEntity(new Message(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), false), httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Message> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        Message errorMessage = new Message(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), false);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(errorMessage);
     }
 }
